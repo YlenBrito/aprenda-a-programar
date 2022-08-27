006e4110528b0e25358c3213187d526f607ce422
Ylen <ylenbritto@gmail.com>
2022-08-27 16:25:12 -0300

    desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,16 @@ let email = "ylenbritto@gmail.com"
 let profissão = "Jornalista"
 let idade = 30
 let lugar = "Amsterdam"
+
+
+console.log("Nome: ", nome)
+console.log("Email: ", email)
+console.log("Profissão: ",profissão)
+console.log("Idade: ", idade)
+console.log("Lugar dos sonhos: ", lugar)
+
+
+//Olá, <valor da variável que tem o seu nome>! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para <valor da variável que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem! :D
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D
+`)
